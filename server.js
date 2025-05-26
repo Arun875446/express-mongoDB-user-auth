a3a547691624b13c4e57b8cb479facc930192d47
arunkumar <arun875446@gmail.com>
2025-05-26 14:57:01 +0530

    cors

--- server.js
+++ server.js
@@ -8,7 +8,10 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173', // frontend origin
+    credentials: true,
+  }));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(users);
