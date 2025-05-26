74ea5d90fb8db5b73b02bd3852e126325ece50a0
arunkumar <arun875446@gmail.com>
2025-05-26 14:38:35 +0530

    added hash

--- routes/usersWithoutHash.js
+++ routes/usersWithoutHash.js
@@ -1,7 +1,6 @@
 import express from "express";
 const router = express.Router();
 import User from "../models/User.js";
-import bcrypt from "bcryptjs";
 
 router.get("/api/users/", async (req, res) => {
   try {
@@ -71,80 +70,32 @@ router.delete("/api/users/:id", async (req, res) => {
   }
 });
 
-// router.put("/api/users/:id", async (req, res) => {
-//   try {
-//     const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {
-//       new: true,
-//     });
-//     if (updatedUser) {
-//       res.status(200).json(updatedUser);
-//     } else {
-//       res.status(404).json({
-//         message: `User with ID ${req.params.id} not found`,
-//       });
-//     }
-//   } catch (error) {
-//     res.status(500).json({ message: `Error feching users, ${error}` });
-//   }
-// });
-
 router.put("/api/users/:id", async (req, res) => {
   try {
-    const updateData = { ...req.body };
-    if (updateData) {
-      const salt = await bcrypt.genSalt(10);
-      updateData.password = await bcrypt.hash(updateData.password, salt);
-    }
-
-    const updatedUser = await User.findByIdAndUpdate(
-      req.params.id,
-      updateData,
-      { new: true }
-    );
-
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
     if (updatedUser) {
       res.status(200).json(updatedUser);
     } else {
-      res
-        .status(400)
-        .json({ message: `User with ID ${req.params.id} not found ` });
+      res.status(404).json({
+        message: `User with ID ${req.params.id} not found`,
+      });
     }
   } catch (error) {
-    res.status(500).json({ message: `Error editing  users, ${error} ` });
+    res.status(500).json({ message: `Error feching users, ${error}` });
   }
 });
 
-// router.post("/api/users/", async (req, res) => {
-//   const newUser = new User({
-//     username: username,
-//     password: req.body.password,
-//     biodata: req.body.biodata,
-//     jobRole: req.body.jobrole,
-//   });
-
-//   try {
-//     const savedUser = await newUser.save();
-//     res.status(200).json(savedUser);
-//   } catch (error) {
-//     res.status(400).json({ message: `Error creating new user: ${error}` });
-//   }
-// });
-
 router.post("/api/users/", async (req, res) => {
-  const { username, password, biodata, jobrole } = req.body;
+  const newUser = new User({
+    username: req.body.username,
+    password: req.body.password,
+    biodata: req.body.biodata,
+    jobRole: req.body.jobrole,
+  });
 
   try {
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
-    console.log(hashedPassword);
-
-    const newUser = new User({
-      username,
-      password: hashedPassword,
-      biodata,
-      jobRole: jobrole,
-    });
-
     const savedUser = await newUser.save();
     res.status(200).json(savedUser);
   } catch (error) {
